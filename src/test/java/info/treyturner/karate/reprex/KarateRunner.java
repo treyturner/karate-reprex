bab14fc5ddb2a016738da665e4ef82901a4ef404
Turner Trey <trey.turner@kapsch.net>
2020-05-29 15:29:10 -0500

    Update Karate to 0.9.6.RC2

--- src/test/java/info/treyturner/karate/reprex/KarateRunner.java
+++ src/test/java/info/treyturner/karate/reprex/KarateRunner.java
@@ -2,15 +2,23 @@ package info.treyturner.karate.reprex;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import org.junit.jupiter.api.Test;
+import com.intuit.karate.junit5.Karate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class KarateRunner {
 
+    // Parallel JUnit 5
     @Test
     void testParallel() {
         int threads = Runtime.getRuntime().availableProcessors() / 2;
-        Results results = Runner.path(".").tags("~@Ignore,~@Reusable").parallel(threads);
+        Results results = Runner.path("classpath:").reportDir("build/reports/surefire").tags("~@Ignore", "~@Reusable").parallel(threads);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
+
+    // Serial JUnit 5
+//    @Karate.Test
+//    Karate testFullPath() {
+//        return Karate.run("classpath:").tags("~@Reusable,~@Ignore");
+//    }
 }
--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,9 +1,6 @@
 function fn() {    
   var env = karate.env // get system property 'karate.env'
-//  karate.log('karate.env system property was:', env)
-  if (!env) {
-    env = 'test'
-  }
+  if (!env) { env = 'test' }
   var config = {
     env: env,
   }
